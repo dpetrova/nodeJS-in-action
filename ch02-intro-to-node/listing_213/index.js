d3971af6132ef9733acc9aff5f8a2e50568e7680
dpetrova <daniella_petrova@abv.bg>
2020-10-02 12:14:37 +0300

    extending Node's event emmiter

--- ch02-intro-to-node/listing_213/index.js
+++ ch02-intro-to-node/listing_213/index.js
@@ -1,16 +1,24 @@
-'use strict';
-const fs = require('fs');
-const Watcher = require('./watcher');
-const watchDir = './watch';
-const processedDir = './done';
+//Use Watcher class to create a utility that watches a directory
+//and renaming any files placed in it to lowercase and then copying the files into a separate directory
+
+"use strict";
+const fs = require("fs");
+const Watcher = require("./watcher");
+const watchDir = "./watch";
+const processedDir = "./done";
 const watcher = new Watcher(watchDir, processedDir);
 
-watcher.on('process', (file) => {
+// use the "on" method, inherited from the event emitter class, to set the logic used to process each file
+watcher.on("process", (file) => {
   const watchFile = `${watchDir}/${file}`;
   const processedFile = `${processedDir}/${file.toLowerCase()}`;
-  fs.rename(watchFile, processedFile, err => {
+  fs.rename(watchFile, processedFile, (err) => {
     if (err) throw err;
   });
 });
 
 watcher.start();
+
+//To test after start the program, create
+//create/drop files into the watch directory,
+//and see the files pop up, renamed to lowercase, in the done directory
--- ch02-intro-to-node/listing_213/watcher.js
+++ ch02-intro-to-node/listing_213/watcher.js
@@ -1,23 +1,28 @@
-'use strict';
-const fs = require('fs');
-const events = require('events');
+//Create a class called Watcher that processes files placed in a specified filesystem directory
 
+"use strict";
+const fs = require("fs");
+const events = require("events");
+
+//Extends EventEmitter with method that processes files
 class Watcher extends events.EventEmitter {
   constructor(watchDir, processedDir) {
-    super();
+    super(); //by calling the super() method in the constructor, we call the parent's constructor and gets access to the parent's properties and methods
     this.watchDir = watchDir;
     this.processedDir = processedDir;
   }
 
+  //processes each file in watch directory
   watch() {
     fs.readdir(this.watchDir, (err, files) => {
       if (err) throw err;
       for (var index in files) {
-        this.emit('process', files[index]);
+        this.emit("process", files[index]);
       }
     });
   }
 
+  // method to start watching
   start() {
     fs.watchFile(this.watchDir, () => {
       this.watch();
