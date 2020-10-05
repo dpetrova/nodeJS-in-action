4b4d78ac4b2d6c0ebb9645a114e478fb09f5b4ec
dpetrova <daniella_petrova@abv.bg>
2020-10-05 16:43:31 +0300

    implememnt serial/parallel flow control

--- ch02-intro-to-node/listing_217/index.js
+++ ch02-intro-to-node/listing_217/index.js
@@ -1,61 +1,66 @@
-'use strict';
-const fs = require('fs');
-const request = require('request');
-const htmlparser = require('htmlparser');
-const configFilename = './rss_feeds.txt';
+//Implementing serial flow control
 
+"use strict";
+const fs = require("fs");
+const request = require("request");
+const htmlparser = require("htmlparser");
+const configFilename = "./rss_feeds.txt";
+
+//Task 1: Make sure the file containing the list of RSS feed URLs exists
 function checkForRSSFile() {
   fs.exists(configFilename, (exists) => {
-    if (!exists)
-      return next(new Error(`Missing RSS file: ${configFilename}`));
+    if (!exists) {
+      return next(new Error(`Missing RSS file: ${configFilename}`)); //Whenever there’s an error, return early
+    }
     next(null, configFilename);
   });
 }
 
+//Task 2: Read and parse the file containing the feed URLs
 function readRSSFile(configFilename) {
   fs.readFile(configFilename, (err, feedList) => {
     if (err) return next(err);
+    //Converts list of feed URLs to a string and then into an array of feed URLs
     feedList = feedList
       .toString()
-      .replace(/^\s+|\s+$/g, '')
-      .split('\n');
+      .replace(/^\s+|\s+$/g, "")
+      .split("\n");
+    //Selects random feed URL from array of feed URLs
     const random = Math.floor(Math.random() * feedList.length);
     next(null, feedList[random]);
   });
 }
 
+//Task 3: Do an HTTP request and get data for the selected feed
 function downloadRSSFeed(feedUrl) {
-  request({uri: feedUrl}, (err, res, body) => {
+  request({ uri: feedUrl }, (err, res, body) => {
     if (err) return next(err);
     if (res.statusCode !== 200)
-      return next(new Error('Abnormal response status code'));
+      return next(new Error("Abnormal response status code"));
     next(null, body);
   });
 }
 
+//Task 4: Parse RSS data into the array of items
 function parseRSSFeed(rss) {
   const handler = new htmlparser.RssHandler();
   const parser = new htmlparser.Parser(handler);
   parser.parseComplete(rss);
-  if (!handler.dom.items.length)
-    return next(new Error('No RSS items found'));
+  if (!handler.dom.items.length) return next(new Error("No RSS items found"));
   const item = handler.dom.items.shift();
   console.log(item.title);
   console.log(item.link);
 }
 
-const tasks = [
-  checkForRSSFile,
-  readRSSFile,
-  downloadRSSFeed,
-  parseRSSFeed
-];
+//Adds each task to be performed to an array in execution order
+const tasks = [checkForRSSFile, readRSSFile, downloadRSSFeed, parseRSSFeed];
 
+//A function called next executes each task
 function next(err, result) {
-  if (err) throw err;
-  const currentTask = tasks.shift();
+  if (err) throw err; //Throws exception if task encounters an error
+  const currentTask = tasks.shift(); //Next task comes from array of tasks
   if (currentTask) {
-    currentTask(result);
+    currentTask(result); //Executes current task
   }
 }
 
--- ch02-intro-to-node/listing_218/index.js
+++ ch02-intro-to-node/listing_218/index.js
@@ -1,12 +1,15 @@
-'use strict';
-const fs = require('fs');
+//Implementing parallel flow control
+
+"use strict";
+const fs = require("fs");
 const tasks = [];
 const wordCounts = {};
-const filesDir = './text';
+const filesDir = "./text";
 let completedTasks = 0;
 
 function checkIfComplete() {
   completedTasks++;
+  //When all tasks have completed, list each word used in the files and the number of times it was used
   if (completedTasks === tasks.length) {
     for (let index in wordCounts) {
       console.log(`${index}: ${wordCounts[index]}`);
@@ -15,26 +18,23 @@ function checkIfComplete() {
 }
 
 function addWordCount(word) {
-  wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1;
+  wordCounts[word] = wordCounts[word] ? wordCounts[word] + 1 : 1;
 }
 
 function countWordsInText(text) {
-  const words = text
-    .toString()
-    .toLowerCase()
-    .split(/\W+/)
-    .sort();
-
-  words
-    .filter(word => word)
-    .forEach(word => addWordCount(word));
+  const words = text.toString().toLowerCase().split(/\W+/).sort();
+  //Counts word occurrences in text
+  words.filter((word) => word).forEach((word) => addWordCount(word));
 }
 
+//Gets a list of the files in the text directory
 fs.readdir(filesDir, (err, files) => {
   if (err) throw err;
 
-  files.forEach(file => {
-    const task = (file => {
+  files.forEach((file) => {
+    //Defines a task to handle each file.
+    //Each task includes a call to a function that will asynchronously read the file and then count the file’s word usage
+    const task = ((file) => {
       return () => {
         fs.readFile(file, (err, text) => {
           if (err) throw err;
@@ -43,8 +43,10 @@ fs.readdir(filesDir, (err, files) => {
         });
       };
     })(`${filesDir}/${file}`);
+    //Adds each task to an array of functions to call in parallel
     tasks.push(task);
   });
 
-  tasks.forEach(task => task());
+  //Starts executing  every task in parallel
+  tasks.forEach((task) => task());
 });
