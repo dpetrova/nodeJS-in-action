38074e1f1590f5da26373817ce0213395c2880b7
dpetrova <daniella_petrova@abv.bg>
2021-01-15 11:53:36 +0200

    Node deployment and maintaining

--- ch10-deployment/listing_10.2_Node-cluster-API-spawn-master-and-workers/index.js
+++ ch10-deployment/listing_10.2_Node-cluster-API-spawn-master-and-workers/index.js
@@ -1,7 +1,13 @@
+/* Demonstration of Node’s cluster API which automatically spawns a master process and a worker for each additional core */
+
 const cluster = require('cluster');
 const http = require('http');
+
+//determines the server’s number of cores
 const numCPUs = require('os').cpus().length;
+
 if (cluster.isMaster) {
+  //creates a fork for each core
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
@@ -9,6 +15,7 @@ if (cluster.isMaster) {
     console.log('Worker %s died.', worker.process.pid);
   });
 } else {
+  //defines work to be done by each worker
   http.Server((req, res) => {
     res.writeHead(200);
     res.end('I am a worker running in process: ' + process.pid);
--- ch10-deployment/listing_10.3_Node-cluster-API-communication-between-master-and-workers/index.js
+++ ch10-deployment/listing_10.3_Node-cluster-API-communication-between-master-and-workers/index.js
@@ -1,3 +1,5 @@
+/* Demonstration of communication between master and workers in Node’s cluster API */
+
 'use strict';
 
 const cluster = require('cluster');
@@ -10,10 +12,13 @@ if (cluster.isMaster) {
   for (let i = 0; i < numCPUs; i++) {
     workers[i] = cluster.fork();
     ((i) => {
+      //listen for messages from worker
       workers[i].on('message', (message) => {
         if (message.cmd == 'incrementRequestTotal') {
+          //increase total requests
           requests++;
           for (var j = 0; j < numCPUs; j++) {
+            //send new total requests to each worker
             workers[j].send({
               cmd: 'updateOfRequestTotal',
               requests: requests
@@ -21,21 +26,26 @@ if (cluster.isMaster) {
           }
         }
       });
-    })(i);
+    })(i); //uses closure to preserve the value of worker
   }
 
   cluster.on('exit', (worker, code, signal) => {
     console.log('Worker %s died.', worker.process.pid);
   });
-} else {
-  process.on('message', (message) => {
+} 
+else {
+  //listen for messages from master
+  process.on('message', (message) => {    
     if (message.cmd === 'updateOfRequestTotal') {
+      //updates request count using master’s message
       requests = message.requests;
     }
   });
+
   http.Server((req, res) => {
     res.writeHead(200);
     res.end(`Worker ${process.pid}: ${requests} requests.`);
+    //lets master know total requests should increase
     process.send({ cmd: 'incrementRequestTotal' });
   }).listen(8000);
 }
